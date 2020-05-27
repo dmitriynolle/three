8d3645bae13dc0f7b0b8068aa1999867ad9152f7
Dmitriy <dimusy@mail.ru>
2020-05-27 15:34:44 +0300

    test

---
+++ src/ru/geekbrains/java_three/Car.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.java_three;
+
+import java.util.concurrent.CyclicBarrier;
+
+import static ru.geekbrains.java_three.Main.CARS_COUNT;
+
+public class Car implements Runnable {
+    private static int COUNT;
+
+    static {
+        COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private static CyclicBarrier collect = new CyclicBarrier(CARS_COUNT);
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        COUNT++;
+        this.name = "Участник #" + COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            collect.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+    }
+}
